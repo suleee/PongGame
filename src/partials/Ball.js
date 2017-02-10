c6ae59b6481cc5dd35ff0c231b027c626b8ec927
Jisu <jaejisu@gmail.com>
2017-02-10 11:21:31 -0800

    add the ball direction/movement

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -14,9 +14,23 @@ export default class Ball {
 reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
+    
+   
+    this.vy = 0;
+    while(this.vy === 0){
+        //when this.vy hit 0 do math again
+        Math.floor(Math.random() * 10 - 5);  //a number between -5 and 5 //direction of the ball
+    }
+
+    this.vx = this.direction * (6 - Math.abs(this.vy)); //abs abosoulte
     }
 
 render(svg){
+    this.x += this.vx;    //this.x = this.x + this.vx;
+    this.y += this.vy;
+
+
+
     let ball = document.createElementNS(SVG_NS, 'circle');
         ball.setAttributeNS(null, 'fill', 'white');
         ball.setAttributeNS(null, 'cx', this.x); //move this.boardWith/2 to reset //this will always in the center
